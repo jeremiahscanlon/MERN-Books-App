3c8b0fb8e225751421b546d18fa315820bc8da5f
Jeremiah Scanlon <jeremiah.j.scanlon@gmail.com>
2019-04-03 12:05:54 -0400

    initial push

---
+++ client/build/precache-manifest.a3368f866e7504417cfffca6970233fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1aa9c6a18e1cbe5375f1",
+    "url": "/static/css/main.f7d43523.chunk.css"
+  },
+  {
+    "revision": "1aa9c6a18e1cbe5375f1",
+    "url": "/static/js/main.552aee44.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "26553e7036fb655a5e00",
+    "url": "/static/js/2.f43e1651.chunk.js"
+  },
+  {
+    "revision": "8c1926e2ab1d5534b6030fc94e8debef",
+    "url": "/index.html"
+  }
+];
